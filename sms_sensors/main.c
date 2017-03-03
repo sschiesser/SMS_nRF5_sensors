fdc0480ae946b2be594095ddeacbec665cdbc6e5
Sebastien Schiesser <sebastien.schiesser@zhdk.ch>
2017-03-03 13:52:21 +0100

    included SPI driver & init function

--- sms_sensors/main.c
+++ sms_sensors/main.c
@@ -32,6 +32,7 @@
 #include "ble_lbs.h"
 #include "bsp.h"
 #include "ble_gap.h"
+#include "nrf_drv_spi.h"
 
 #define NRF_LOG_MODULE_NAME "APP"
 #include "nrf_log.h"
@@ -78,6 +79,14 @@
 
 static uint16_t                         m_conn_handle = BLE_CONN_HANDLE_INVALID;    /**< Handle of the current connection. */
 static ble_lbs_t                        m_lbs;                                      /**< LED Button Service instance. */
+
+#define SPI_INSTANCE  0 /**< SPI instance index. */
+static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);  /**< SPI instance. */
+static volatile bool spi_xfer_done;  /**< Flag used to indicate that SPI instance completed the transfer. */
+#define TEST_STRING "Nordic"
+static uint8_t       m_tx_buf[] = TEST_STRING;           /**< TX buffer. */
+static uint8_t       m_rx_buf[sizeof(TEST_STRING) + 1];    /**< RX buffer. */
+static const uint8_t m_length = sizeof(m_tx_buf);        /**< Transfer length. */
 
 
 /**@brief Function for assert macro callback.
@@ -510,6 +519,29 @@ static void buttons_init(void)
     APP_ERROR_CHECK(err_code);
 }
 
+void spi_event_handler(nrf_drv_spi_evt_t const * p_event)
+{
+    spi_xfer_done = true;
+    NRF_LOG_INFO("Transfer completed.\r\n");
+    if (m_rx_buf[0] != 0)
+    {
+        NRF_LOG_INFO(" Received: \r\n");
+        NRF_LOG_HEXDUMP_INFO(m_rx_buf, strlen((const char *)m_rx_buf));
+    }
+}
+
+
+/**@brief Function for initializing the SPI module.
+ */
+static void spi_init(void)
+{
+	nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
+	spi_config.ss_pin = SPI_SS_PIN;
+	spi_config.miso_pin = SPI_MISO_PIN;
+	spi_config.mosi_pin = SPI_MOSI_PIN;
+	spi_config.sck_pin = SPI_SCK_PIN;
+	APP_ERROR_CHECK(nrf_drv_spi_init(&spi, &spi_config, spi_event_handler));
+}
 
 /**@brief Function for the Power Manager.
  */
