5a8dfd3dab16310fbdf008e1330d446a824f149e
Sebastien Schiesser <sebastien.schiesser@zhdk.ch>
2017-03-07 15:43:06 +0100

    TWI initialized, writeByte/readByte adapted...

--- sms_sensors/main.c
+++ sms_sensors/main.c
@@ -35,6 +35,7 @@
 #include "ble_gap.h"
 #include "nrf_drv_spi.h"
 #include "ms58.h"
+#include "mpu9250.h"
 #include "nrf_delay.h"
 #include "SEGGER_RTT.h"
 #include "nrf_drv_twi.h"
@@ -98,6 +99,8 @@ struct ms58_output_s					ms58_output;
 static const nrf_drv_twi_t twi_master_instance = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE);
 static volatile bool twi_xfer_done;
 
+struct mpu9250_config_s					mpu9250_config;
+struct mpu9250_output_s					mpu9250_output;
 
 APP_TIMER_DEF(periph_init_timer_id);
 static volatile bool periph_init_timer_done;
@@ -559,6 +562,7 @@ void spi_event_handler(nrf_drv_spi_evt_t const * p_event)
 
 void twi_event_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
 {
+	twi_xfer_done = true;
 	switch(p_event->type)
 	{
 		case NRF_DRV_TWI_EVT_DONE:
@@ -602,6 +606,47 @@ static void twi_init(void)
 		
 		nrf_drv_twi_enable(&twi_master_instance);
 }
+
+void writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+	uint8_t reg[2] = {subAddress, data};
+	twi_xfer_done = false;
+	ret_code_t err_code = nrf_drv_twi_tx(&twi_master_instance, address, reg, sizeof(reg), false);
+	APP_ERROR_CHECK(err_code);
+	while(!twi_xfer_done) {};
+}
+
+uint8_t readByte(uint8_t address, uint8_t subAddress)
+{
+	uint8_t reg[1] = {subAddress};
+	twi_xfer_done = false;
+	ret_code_t err_code = nrf_drv_twi_tx(&twi_master_instance, address, reg, sizeof(reg), false);
+	APP_ERROR_CHECK(err_code);
+	while(!twi_xfer_done);
+		
+	uint8_t data;
+	twi_xfer_done = false;
+	err_code = nrf_drv_twi_rx(&twi_master_instance, address, &data, sizeof(data));
+	APP_ERROR_CHECK(err_code);
+	while(!twi_xfer_done);
+	
+	return data;
+}
+
+void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t *dest)
+{
+	uint8_t reg[1] = {subAddress};
+	twi_xfer_done = false;
+	ret_code_t err_code = nrf_drv_twi_tx(&twi_master_instance, address, reg, sizeof(reg), false);
+	APP_ERROR_CHECK(err_code);
+	while(!twi_xfer_done);
+	
+	twi_xfer_done = false;
+	err_code = nrf_drv_twi_rx(&twi_master_instance, address, dest, count);
+	APP_ERROR_CHECK(err_code);
+	while(!twi_xfer_done);
+}
+
 
 /**@brief Function for the Power Manager.
  */
@@ -783,6 +828,179 @@ static void ms58_calculate(void)
 }
 
 
+static int mpu9250_check(void)
+{
+	int ret = -1;
+	twi_xfer_done = false;
+	uint8_t reg[1] = {WHO_AM_I_MPU9250};
+	ret_code_t err_code = nrf_drv_twi_tx(&twi_master_instance, MPU9250_ADDRESS, reg, sizeof(reg), false);
+	APP_ERROR_CHECK(err_code);
+	while(!twi_xfer_done) {};
+		
+	uint8_t read_sample;
+	twi_xfer_done = false;
+	err_code = nrf_drv_twi_rx(&twi_master_instance, MPU9250_ADDRESS, &read_sample, sizeof(read_sample));
+	APP_ERROR_CHECK(err_code);
+	while(!twi_xfer_done) {};
+	if(read_sample == 0x71) ret = 0;
+		
+	return ret;
+}
+
+
+static void mpu9250_calibrate(float *dest1, float *dest2)
+{
+	uint8_t data[12]; // data array to hold accelerometer and gyro x, y, z, data
+	uint16_t ii, packet_count, fifo_count;
+	int32_t gyro_bias[3]  = {0, 0, 0};
+	int32_t accel_bias[3] = {0, 0, 0};
+	
+	// reset device
+	writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x80); // Write a one to bit 7 reset bit; toggle reset device
+	nrf_delay_ms(100);
+
+	// get stable time source; Auto select clock source to be PLL gyroscope reference if ready
+	// else use the internal oscillator, bits 2:0 = 001
+	writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x01);
+	writeByte(MPU9250_ADDRESS, PWR_MGMT_2, 0x00);
+	nrf_delay_ms(200);
+
+	/*	
+	// Configure device for bias calculation
+	writeByte(MPU9250_ADDRESS, INT_ENABLE, 0x00);   // Disable all interrupts
+	writeByte(MPU9250_ADDRESS, FIFO_EN, 0x00);      // Disable FIFO
+	writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x00);   // Turn on internal clock source
+	writeByte(MPU9250_ADDRESS, I2C_MST_CTRL, 0x00); // Disable I2C master
+	writeByte(MPU9250_ADDRESS, USER_CTRL, 0x00);    // Disable FIFO and I2C master modes
+	writeByte(MPU9250_ADDRESS, USER_CTRL, 0x0C);    // Reset FIFO and DMP
+	nrf_delay_ms(15);
+	
+	// Configure MPU9250 gyro and accelerometer for bias calculation
+	writeByte(MPU9250_ADDRESS, CONFIG, 0x01);      // Set low-pass filter to 188 Hz
+	writeByte(MPU9250_ADDRESS, SMPLRT_DIV, 0x00);  // Set sample rate to 1 kHz
+	writeByte(MPU9250_ADDRESS, GYRO_CONFIG, 0x00);  // Set gyro full-scale to 250 degrees per second, maximum sensitivity
+	writeByte(MPU9250_ADDRESS, ACCEL_CONFIG, 0x00); // Set accelerometer full-scale to 2 g, maximum sensitivity
+	
+	uint16_t  gyrosensitivity  = 131;   // = 131 LSB/degrees/sec
+	uint16_t  accelsensitivity = 16384;  // = 16384 LSB/g
+
+	// Configure FIFO to capture accelerometer and gyro data for bias calculation
+	writeByte(MPU9250_ADDRESS, USER_CTRL, 0x40);   // Enable FIFO
+	writeByte(MPU9250_ADDRESS, FIFO_EN, 0x78);     // Enable gyro and accelerometer sensors for FIFO  (max size 512 bytes in MPU-9150)
+	nrf_delay_ms(40); // accumulate 40 samples in 40 milliseconds = 480 bytes
+
+	// At end of sample accumulation, turn off FIFO sensor read
+	writeByte(MPU9250_ADDRESS, FIFO_EN, 0x00);        // Disable gyro and accelerometer sensors for FIFO
+	readBytes(MPU9250_ADDRESS, FIFO_COUNTH, 2, data); // read FIFO sample count
+	fifo_count = ((uint16_t)data[0] << 8) | data[1];
+	packet_count = fifo_count/12;// How many sets of full gyro and accelerometer data for averaging
+	
+	for (ii = 0; ii < packet_count; ii++) {
+		int16_t accel_temp[3] = {0, 0, 0}, gyro_temp[3] = {0, 0, 0};
+		readBytes(MPU9250_ADDRESS, FIFO_R_W, 12, data); // read data for averaging
+		accel_temp[0] = (int16_t) (((int16_t)data[0] << 8) | data[1]  ) ;  // Form signed 16-bit integer for each sample in FIFO
+		accel_temp[1] = (int16_t) (((int16_t)data[2] << 8) | data[3]  ) ;
+		accel_temp[2] = (int16_t) (((int16_t)data[4] << 8) | data[5]  ) ;
+		gyro_temp[0]  = (int16_t) (((int16_t)data[6] << 8) | data[7]  ) ;
+		gyro_temp[1]  = (int16_t) (((int16_t)data[8] << 8) | data[9]  ) ;
+		gyro_temp[2]  = (int16_t) (((int16_t)data[10] << 8) | data[11]) ;
+		
+		accel_bias[0] += (int32_t) accel_temp[0]; // Sum individual signed 16-bit biases to get accumulated signed 32-bit biases
+		accel_bias[1] += (int32_t) accel_temp[1];
+		accel_bias[2] += (int32_t) accel_temp[2];
+		gyro_bias[0]  += (int32_t) gyro_temp[0];
+		gyro_bias[1]  += (int32_t) gyro_temp[1];
+		gyro_bias[2]  += (int32_t) gyro_temp[2];
+		
+	}
+	accel_bias[0] /= (int32_t) packet_count; // Normalize sums to get average count biases
+	accel_bias[1] /= (int32_t) packet_count;
+	accel_bias[2] /= (int32_t) packet_count;
+	gyro_bias[0]  /= (int32_t) packet_count;
+	gyro_bias[1]  /= (int32_t) packet_count;
+	gyro_bias[2]  /= (int32_t) packet_count;
+	
+	if(accel_bias[2] > 0L) {
+		accel_bias[2] -= (int32_t) accelsensitivity;
+	}  // Remove gravity from the z-axis accelerometer bias calculation
+	else {
+		accel_bias[2] += (int32_t) accelsensitivity;
+	}
+	
+	// Construct the gyro biases for push to the hardware gyro bias registers, which are reset to zero upon device startup
+	data[0] = (-gyro_bias[0]/4  >> 8) & 0xFF; // Divide by 4 to get 32.9 LSB per deg/s to conform to expected bias input format
+	data[1] = (-gyro_bias[0]/4)       & 0xFF; // Biases are additive, so change sign on calculated average gyro biases
+	data[2] = (-gyro_bias[1]/4  >> 8) & 0xFF;
+	data[3] = (-gyro_bias[1]/4)       & 0xFF;
+	data[4] = (-gyro_bias[2]/4  >> 8) & 0xFF;
+	data[5] = (-gyro_bias[2]/4)       & 0xFF;
+	
+	// Push gyro biases to hardware registers
+	writeByte(MPU9250_ADDRESS, XG_OFFSET_H, data[0]);
+	writeByte(MPU9250_ADDRESS, XG_OFFSET_L, data[1]);
+	writeByte(MPU9250_ADDRESS, YG_OFFSET_H, data[2]);
+	writeByte(MPU9250_ADDRESS, YG_OFFSET_L, data[3]);
+	writeByte(MPU9250_ADDRESS, ZG_OFFSET_H, data[4]);
+	writeByte(MPU9250_ADDRESS, ZG_OFFSET_L, data[5]);
+	
+	// Output scaled gyro biases for display in the main program
+	dest1[0] = (float)((float)gyro_bias[0]/(float)gyrosensitivity);
+	dest1[1] = (float)((float)gyro_bias[1]/(float)gyrosensitivity);
+	dest1[2] = (float)((float)gyro_bias[2]/(float)gyrosensitivity);
+
+
+	// Construct the accelerometer biases for push to the hardware accelerometer bias registers. These registers contain
+	// factory trim values which must be added to the calculated accelerometer biases; on boot up these registers will hold
+	// non-zero values. In addition, bit 0 of the lower byte must be preserved since it is used for temperature
+	// compensation calculations. Accelerometer bias registers expect bias input as 2048 LSB per g, so that
+	// the accelerometer biases calculated above must be divided by 8.
+
+	int32_t accel_bias_reg[3] = {0, 0, 0}; // A place to hold the factory accelerometer trim biases
+	readBytes(MPU9250_ADDRESS, XA_OFFSET_H, 2, data); // Read factory accelerometer trim values
+	accel_bias_reg[0] = (int32_t) (((int16_t)data[0] << 8) | data[1]);
+	readBytes(MPU9250_ADDRESS, YA_OFFSET_H, 2, data);
+	accel_bias_reg[1] = (int32_t) (((int16_t)data[0] << 8) | data[1]);
+	readBytes(MPU9250_ADDRESS, ZA_OFFSET_H, 2, data);
+	accel_bias_reg[2] = (int32_t) (((int16_t)data[0] << 8) | data[1]);
+	
+	uint32_t mask = 1uL; // Define mask for temperature compensation bit 0 of lower byte of accelerometer bias registers
+	uint8_t mask_bit[3] = {0, 0, 0}; // Define array to hold mask bit for each accelerometer bias axis
+	
+	for(ii = 0; ii < 3; ii++) {
+		if((accel_bias_reg[ii] & mask)) mask_bit[ii] = 0x01; // If temperature compensation bit is set, record that fact in mask_bit
+	}
+	
+	// Construct total accelerometer bias, including calculated average accelerometer bias from above
+	accel_bias_reg[0] -= (accel_bias[0]/8); // Subtract calculated averaged accelerometer bias scaled to 2048 LSB/g (16 g full scale)
+	accel_bias_reg[1] -= (accel_bias[1]/8);
+	accel_bias_reg[2] -= (accel_bias[2]/8);
+	
+	data[0] = (accel_bias_reg[0] >> 8) & 0xFF;
+	data[1] = (accel_bias_reg[0])      & 0xFF;
+	data[1] = data[1] | mask_bit[0]; // preserve temperature compensation bit when writing back to accelerometer bias registers
+	data[2] = (accel_bias_reg[1] >> 8) & 0xFF;
+	data[3] = (accel_bias_reg[1])      & 0xFF;
+	data[3] = data[3] | mask_bit[1]; // preserve temperature compensation bit when writing back to accelerometer bias registers
+	data[4] = (accel_bias_reg[2] >> 8) & 0xFF;
+	data[5] = (accel_bias_reg[2])      & 0xFF;
+	data[5] = data[5] | mask_bit[2]; // preserve temperature compensation bit when writing back to accelerometer bias registers
+	
+	// Apparently this is not working for the acceleration biases in the MPU-9250
+	// Are we handling the temperature correction bit properly?
+	// Push accelerometer biases to hardware registers
+	writeByte(MPU9250_ADDRESS, XA_OFFSET_H, data[0]);
+	writeByte(MPU9250_ADDRESS, XA_OFFSET_L, data[1]);
+	writeByte(MPU9250_ADDRESS, YA_OFFSET_H, data[2]);
+	writeByte(MPU9250_ADDRESS, YA_OFFSET_L, data[3]);
+	writeByte(MPU9250_ADDRESS, ZA_OFFSET_H, data[4]);
+	writeByte(MPU9250_ADDRESS, ZA_OFFSET_L, data[5]);
+
+	// Output scaled accelerometer biases for display in the main program
+	dest2[0] = (float)accel_bias[0]/(float)accelsensitivity;
+	dest2[1] = (float)accel_bias[1]/(float)accelsensitivity;
+	dest2[2] = (float)accel_bias[2]/(float)accelsensitivity;
+	*/
+}
 /**@brief Function for application main entry.
  */
 int main(void)
@@ -806,28 +1024,33 @@ int main(void)
 	spi_init();
 	twi_init();
 	
+	if(!mpu9250_check()){
+		SEGGER_RTT_printf(0, "mpu9250 is present");
+		mpu9250_calibrate(mpu9250_config.gyro_bias, mpu9250_config.accel_bias);
+	}
+	
 	ms58_reset();
 	ms58_read_prom();
 	ms58_output.complete = false;
 	
-	while(1)
-	{
-		nrf_delay_ms(100);
-		bsp_board_led_on(LEDBUTTON_LED_PIN);
-		
-		ms58_read_data();
-		if(ms58_output.complete) {
-			ms58_calculate();
-			SEGGER_RTT_printf(0, "Pressure: %ld, Temperature: %ld\n", ms58_output.pressure, ms58_output.temperature);
-			ms58_output.complete = false;
-		}
-		else {
-			ms58_output.complete = true;
-		}
-		
-		nrf_delay_ms(50);
-		bsp_board_led_off(LEDBUTTON_LED_PIN);
-	}
+//	while(1)
+//	{
+//		nrf_delay_ms(100);
+//		bsp_board_led_on(LEDBUTTON_LED_PIN);
+//		
+//		ms58_read_data();
+//		if(ms58_output.complete) {
+//			ms58_calculate();
+////			SEGGER_RTT_printf(0, "Pressure: %ld, Temperature: %ld\n", ms58_output.pressure, ms58_output.temperature);
+//			ms58_output.complete = false;
+//		}
+//		else {
+//			ms58_output.complete = true;
+//		}
+//		
+//		nrf_delay_ms(50);
+//		bsp_board_led_off(LEDBUTTON_LED_PIN);
+//	}
 
     // Start execution.
     NRF_LOG_INFO("Blinky Start!\r\n");
