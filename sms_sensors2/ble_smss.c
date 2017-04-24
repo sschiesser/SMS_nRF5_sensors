563f1416db54b97d459e7f34f12167ab11ab82b4
Sebastien Schiesser <sebastien.schiesser@zhdk.ch>
2017-04-24 19:57:10 +0200

    sms_sensors2: added 3 characteristics

--- sms_sensors2/ble_smss.c
+++ sms_sensors2/ble_smss.c
@@ -70,10 +70,128 @@ void ble_smss_on_ble_evt(ble_smss_t * p_smss, ble_evt_t * p_ble_evt)
 
 static uint32_t press_char_add(ble_smss_t * p_smss)
 {
+    uint32_t   err_code = 0; // Variable to hold return codes from library and softdevice functions
+    
+    // OUR_JOB: Step 2.A, Add a custom characteristic UUID
+    ble_uuid_t          char_uuid;
+    ble_uuid128_t       base_uuid = SMSS_UUID_BASE;
+    char_uuid.uuid      = SMSS_UUID_PRESS_CHAR;
+    sd_ble_uuid_vs_add(&base_uuid, &char_uuid.type);
+    APP_ERROR_CHECK(err_code);
+    
+    // OUR_JOB: Step 2.F Add read/write properties to our characteristic
+    ble_gatts_char_md_t char_md;
+    memset(&char_md, 0, sizeof(char_md));
+    char_md.char_props.read = 1;
+    char_md.char_props.write = 1;
+
+    
+    // OUR_JOB: Step 3.A, Configuring Client Characteristic Configuration Descriptor metadata and add to char_md structure
+    ble_gatts_attr_md_t cccd_md;
+    memset(&cccd_md, 0, sizeof(cccd_md));
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
+    cccd_md.vloc                = BLE_GATTS_VLOC_STACK;    
+    char_md.p_cccd_md           = &cccd_md;
+    char_md.char_props.notify   = 1;
+   
+    
+    // OUR_JOB: Step 2.B, Configure the attribute metadata
+    ble_gatts_attr_md_t attr_md;
+    memset(&attr_md, 0, sizeof(attr_md)); 
+    attr_md.vloc        = BLE_GATTS_VLOC_STACK;   
+    
+    
+    // OUR_JOB: Step 2.G, Set read/write security levels to our characteristic
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+    
+    
+    // OUR_JOB: Step 2.C, Configure the characteristic value attribute
+    ble_gatts_attr_t    attr_char_value;
+    memset(&attr_char_value, 0, sizeof(attr_char_value));        
+    attr_char_value.p_uuid      = &char_uuid;
+    attr_char_value.p_attr_md   = &attr_md;
+    
+    // OUR_JOB: Step 2.H, Set characteristic length in number of bytes
+    attr_char_value.max_len     = 12;
+    attr_char_value.init_len    = 12;
+    uint8_t value[12]            = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB};
+    attr_char_value.p_value     = value;
+
+    // OUR_JOB: Step 2.E, Add our new characteristic to the service
+    err_code = sd_ble_gatts_characteristic_add(p_smss->service_handle,
+                                       &char_md,
+                                       &attr_char_value,
+                                       &p_smss->press_char_handles);
+    APP_ERROR_CHECK(err_code);
+    
+    NRF_LOG_INFO("\r\nService handle: %#x\n\r", p_smss->service_handle);
+    NRF_LOG_INFO("Char value handle: %#x\r\n", p_smss->press_char_handles.value_handle);
+    NRF_LOG_INFO("Char cccd handle: %#x\r\n\r\n", p_smss->press_char_handles.cccd_handle);
+
+    return NRF_SUCCESS;
 }
 
 static uint32_t imu_char_add(ble_smss_t * p_smss)
 {
+	uint32_t			err_code = 0;
+	
+	// Add custom characteristic UUID (2.A)
+	ble_uuid_t			char_uuid;
+	ble_uuid128_t		base_uuid = SMSS_UUID_BASE;
+	char_uuid.uuid = SMSS_UUID_IMU_CHAR;
+	sd_ble_uuid_vs_add(&base_uuid, &char_uuid.type);
+	APP_ERROR_CHECK(err_code);
+	
+	// Add read/write properties to characteristic (2.F)
+	ble_gatts_char_md_t	char_md;
+	memset(&char_md, 0, sizeof(char_md));
+	char_md.char_props.read = 1;
+	char_md.char_props.write = 1;
+	
+	// Configure CCCD (3.A)
+	ble_gatts_attr_md_t	cccd_md;
+	memset(&cccd_md, 0, sizeof(cccd_md));
+	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
+	cccd_md.vloc = BLE_GATTS_VLOC_STACK;
+	char_md.p_cccd_md = &cccd_md;
+	char_md.char_props.notify = 1;
+	
+	// Configure attribute metadata (2.B)
+	ble_gatts_attr_md_t	attr_md;
+	memset(&attr_md, 0, sizeof(attr_md));
+	attr_md.vloc = BLE_GATTS_VLOC_STACK;
+	
+	// Set read/write security level to characteristic (2.G)
+	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+	
+	// Configure characteristic value attribute (2.C)
+	ble_gatts_attr_t	attr_char_value;
+	memset(&attr_char_value, 0, sizeof(attr_char_value));
+	attr_char_value.p_uuid = &char_uuid;
+	attr_char_value.p_attr_md = &attr_md;
+	
+	// Set characteristic length (2.H)
+	attr_char_value.max_len = 2;
+	attr_char_value.init_len = 2;
+	uint8_t value[2] = {0, 0};
+	attr_char_value.p_value = value;
+	
+	// Add new characteristic to the service
+	err_code = sd_ble_gatts_characteristic_add(p_smss->service_handle,
+												&char_md,
+												&attr_char_value,
+												&p_smss->imu_char_handles);
+	APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_INFO("\r\nService handle: %#x\n\r", p_smss->service_handle);
+    NRF_LOG_INFO("Char value handle: %#x\r\n", p_smss->imu_char_handles.value_handle);
+    NRF_LOG_INFO("Char cccd handle: %#x\r\n\r\n", p_smss->imu_char_handles.cccd_handle);
+	
+	return NRF_SUCCESS;
 }
 
 
@@ -138,6 +256,10 @@ static uint32_t button_char_add(ble_smss_t * p_smss)
 												&attr_char_value,
 												&p_smss->button_char_handles);
 	APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_INFO("\r\nService handle: %#x\n\r", p_smss->service_handle);
+    NRF_LOG_INFO("Char value handle: %#x\r\n", p_smss->button_char_handles.value_handle);
+    NRF_LOG_INFO("Char cccd handle: %#x\r\n\r\n", p_smss->button_char_handles.cccd_handle);
 	
 	return NRF_SUCCESS;
 }
@@ -165,7 +287,8 @@ void ble_smss_init(ble_smss_t * p_smss)
     
     // OUR_JOB: Call the function our_char_add() to add our new characteristic to the service. 
     button_char_add(p_smss);
-//	our_2ndchar_add(p_our_service);
+	press_char_add(p_smss);
+	imu_char_add(p_smss);
 //    uint32_t   			err_code;
 //	
 //	// Declare base and 16-bis service UUID
