9b3f85c7dddcc849a8f8bab093140621e3f5e33e
Sebastien Schiesser <sebastien.schiesser@zhdk.ch>
2017-04-24 19:51:13 +0200

    sms_sensors2: sucessfully advertised 1st custom characteristic

---
+++ sms_sensors2/ble_smss.c
@@ -0,0 +1,224 @@
+/* Copyright (c) 2013 Nordic Semiconductor. All Rights Reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the license.txt file.
+ */
+#include "sdk_common.h"
+#include "ble_smss.h"
+#include "ble_srv_common.h"
+#include "nrf_log.h"
+#include "app_error.h"
+
+
+/**@brief Function for handling the Connect event.
+ *
+ * @param[in] p_lbs      LED Button Service structure.
+ * @param[in] p_ble_evt  Event received from the BLE stack.
+ */
+static void on_connect(ble_smss_t * p_smss, ble_evt_t * p_ble_evt)
+{
+    p_smss->conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+}
+
+
+/**@brief Function for handling the Disconnect event.
+ *
+ * @param[in] p_lbs      LED Button Service structure.
+ * @param[in] p_ble_evt  Event received from the BLE stack.
+ */
+static void on_disconnect(ble_smss_t * p_smss, ble_evt_t * p_ble_evt)
+{
+    UNUSED_PARAMETER(p_ble_evt);
+    p_smss->conn_handle = BLE_CONN_HANDLE_INVALID;
+}
+
+
+/**@brief Function for handling the Write event.
+ *
+ * @param[in] p_lbs      LED Button Service structure.
+ * @param[in] p_ble_evt  Event received from the BLE stack.
+ */
+static void on_write(ble_smss_t * p_smss, ble_evt_t * p_ble_evt)
+{
+    ble_gatts_evt_write_t * p_evt_write = &p_ble_evt->evt.gatts_evt.params.write;
+}
+
+
+void ble_smss_on_ble_evt(ble_smss_t * p_smss, ble_evt_t * p_ble_evt)
+{
+    switch (p_ble_evt->header.evt_id)
+    {
+        case BLE_GAP_EVT_CONNECTED:
+            on_connect(p_smss, p_ble_evt);
+            break;
+
+        case BLE_GAP_EVT_DISCONNECTED:
+            on_disconnect(p_smss, p_ble_evt);
+            break;
+
+        case BLE_GATTS_EVT_WRITE:
+            on_write(p_smss, p_ble_evt);
+            break;
+
+        default:
+            // No implementation needed.
+            break;
+    }
+}
+
+
+
+static uint32_t press_char_add(ble_smss_t * p_smss)
+{
+}
+
+static uint32_t imu_char_add(ble_smss_t * p_smss)
+{
+}
+
+
+
+/**@brief Function for adding the Button Characteristic.
+ *
+ * @param[in] p_lbs      LED Button Service structure.
+ * @param[in] p_lbs_init LED Button Service initialization structure.
+ *
+ * @retval NRF_SUCCESS on success, else an error value from the SoftDevice
+ */
+static uint32_t button_char_add(ble_smss_t * p_smss)
+{
+	uint32_t			err_code = 0;
+	
+	// Add custom characteristic UUID (2.A)
+	ble_uuid_t			char_uuid;
+	ble_uuid128_t		base_uuid = SMSS_UUID_BASE;
+	char_uuid.uuid = SMSS_UUID_BUTTON_CHAR;
+	sd_ble_uuid_vs_add(&base_uuid, &char_uuid.type);
+	APP_ERROR_CHECK(err_code);
+	
+	// Add read/write properties to characteristic (2.F)
+	ble_gatts_char_md_t	char_md;
+	memset(&char_md, 0, sizeof(char_md));
+	char_md.char_props.read = 1;
+	char_md.char_props.write = 1;
+	
+	// Configure CCCD (3.A)
+	ble_gatts_attr_md_t	cccd_md;
+	memset(&cccd_md, 0, sizeof(cccd_md));
+	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
+	cccd_md.vloc = BLE_GATTS_VLOC_STACK;
+	char_md.p_cccd_md = &cccd_md;
+	char_md.char_props.notify = 1;
+	
+	// Configure attribute metadata (2.B)
+	ble_gatts_attr_md_t	attr_md;
+	memset(&attr_md, 0, sizeof(attr_md));
+	attr_md.vloc = BLE_GATTS_VLOC_STACK;
+	
+	// Set read/write security level to characteristic (2.G)
+	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+	
+	// Configure characteristic value attribute (2.C)
+	ble_gatts_attr_t	attr_char_value;
+	memset(&attr_char_value, 0, sizeof(attr_char_value));
+	attr_char_value.p_uuid = &char_uuid;
+	attr_char_value.p_attr_md = &attr_md;
+	
+	// Set characteristic length (2.H)
+	attr_char_value.max_len = 2;
+	attr_char_value.init_len = 2;
+	uint8_t value[2] = {0, 0};
+	attr_char_value.p_value = value;
+	
+	// Add new characteristic to the service
+	err_code = sd_ble_gatts_characteristic_add(p_smss->service_handle,
+												&char_md,
+												&attr_char_value,
+												&p_smss->button_char_handles);
+	APP_ERROR_CHECK(err_code);
+	
+	return NRF_SUCCESS;
+}
+
+void ble_smss_init(ble_smss_t * p_smss)
+{
+    uint32_t   err_code; // Variable to hold return codes from library and softdevice functions
+
+    // FROM_SERVICE_TUTORIAL: Declare 16-bit service and 128-bit base UUIDs and add them to the BLE stack
+    ble_uuid_t        service_uuid;
+    ble_uuid128_t     base_uuid = SMSS_UUID_BASE;
+    service_uuid.uuid = SMSS_UUID_SERVICE;
+    err_code = sd_ble_uuid_vs_add(&base_uuid, &service_uuid.type);
+    APP_ERROR_CHECK(err_code);    
+    
+    // OUR_JOB: Step 3.B, Set our service connection handle to default value. I.e. an invalid handle since we are not yet in a connection.
+    p_smss->conn_handle = BLE_CONN_HANDLE_INVALID;
+
+    // FROM_SERVICE_TUTORIAL: Add our service
+    err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY,
+                                        &service_uuid,
+                                        &p_smss->service_handle);
+    
+    APP_ERROR_CHECK(err_code);
+    
+    // OUR_JOB: Call the function our_char_add() to add our new characteristic to the service. 
+    button_char_add(p_smss);
+//	our_2ndchar_add(p_our_service);
+//    uint32_t   			err_code;
+//	
+//	// Declare base and 16-bis service UUID
+//	ble_uuid_t			service_uuid;
+//	ble_uuid128_t		base_uuid = SMSS_UUID_BASE;
+//	service_uuid.uuid = SMSS_UUID_SERVICE;
+//	err_code = sd_ble_uuid_vs_add(&base_uuid, &service_uuid.type);
+//	APP_ERROR_CHECK(err_code);
+//	
+//	// Set service connection handle to default value
+//	p_smss->conn_handle = BLE_CONN_HANDLE_INVALID;
+//	
+//	// Add service
+//	err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY,
+//										&service_uuid,
+//										&p_smss->service_handle);
+//	APP_ERROR_CHECK(err_code);
+//	
+//	// Add new characteristics
+//	button_char_add(p_smss);
+}
+
+uint32_t ble_smss_on_button_change(ble_smss_t * p_smss, uint8_t button_state)
+{
+	NRF_LOG_INFO("on button change...\r\n");
+    ble_gatts_hvx_params_t params;
+    uint16_t len = sizeof(button_state);
+
+    memset(&params, 0, sizeof(params));
+    params.type = BLE_GATT_HVX_NOTIFICATION;
+    params.handle = p_smss->button_char_handles.value_handle;
+//    params.handle = p_smss->press_char_handles.value_handle;
+    params.p_data = &button_state;
+    params.p_len = &len;
+
+	NRF_LOG_INFO("Sending: 0x%02x to 0x%04x->0x%04x\n", params.p_data[0], p_smss->conn_handle, params.handle);
+    return sd_ble_gatts_hvx(p_smss->conn_handle, &params);
+}
+
+uint32_t ble_smss_on_press_value(ble_smss_t * p_smss, uint8_t value)
+{
+	NRF_LOG_INFO("on press value...\r\n");
+	ble_gatts_hvx_params_t params;
+	uint16_t len = sizeof(value);
+//	uint8_t content = value;
+	
+	memset(&params, 0, sizeof(params));
+	params.type = BLE_GATT_HVX_NOTIFICATION;
+	params.handle = p_smss->press_char_handles.value_handle;
+	params.p_data = &value;
+	params.p_len = &len;
+	
+	NRF_LOG_INFO("Sending: 0x%02x to 0x%04x->0x%04x\n", params.p_data[0], p_smss->conn_handle, params.handle);
+//	return NRF_SUCCESS;
+	return sd_ble_gatts_hvx(p_smss->conn_handle, &params);
+}
---
+++ sms_sensors2/ble_smss.h
@@ -0,0 +1,116 @@
+/* Copyright (c) 2015 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+/** @file
+ *
+ * @defgroup ble_lbs LED Button Service Server
+ * @{
+ * @ingroup ble_sdk_srv
+ *
+ * @brief LED Button Service Server module.
+ *
+ * @details This module implements a custom LED Button Service with an LED and Button Characteristics.
+ *          During initialization, the module adds the LED Button Service and Characteristics
+ *          to the BLE stack database.
+ *
+ *          The application must supply an event handler for receiving LED Button Service
+ *          events. Using this handler, the service notifies the application when the
+ *          LED value changes.
+ *
+ *          The service also provides a function for letting the application notify
+ *          the state of the Button Characteristic to connected peers.
+ *
+ * @note The application must propagate BLE stack events to the LED Button Service
+ *       module by calling ble_lbs_on_ble_evt() from the @ref softdevice_handler callback.
+*/
+
+#ifndef BLE_SMSS_H__
+#define BLE_SMSS_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "ble.h"
+#include "ble_srv_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SMSS_UUID_BASE        {0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15, \
+                              0xDE, 0xEF, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00}
+#define SMSS_UUID_SERVICE     0x1C57
+#define SMSS_UUID_BUTTON_CHAR 0x1C58
+#define SMSS_UUID_PRESS_CHAR  0x1C59
+#define SMSS_UUID_IMU_CHAR	  0x1C5A
+//#define SMSS_UUID_SERVICE     0x1523
+//#define SMSS_UUID_BUTTON_CHAR 0x1524
+//#define SMSS_UUID_LED_CHAR    0x1525
+//#define SMSS_UUID_PRESS_CHAR  0x1526
+//#define SMSS_UUID_IMU_CHAR	  0x1527
+
+/**@brief LED Button Service structure. This structure contains various status information for the service. */
+typedef struct
+{
+    uint16_t                    conn_handle;         /**< Handle of the current connection (as provided by the BLE stack). BLE_CONN_HANDLE_INVALID if not in a connection. */
+    uint16_t                    service_handle;      /**< Handle of LED Button Service (as provided by the BLE stack). */
+    ble_gatts_char_handles_t    button_char_handles; /**< Handles related to the Button Characteristic. */
+	ble_gatts_char_handles_t	press_char_handles;
+	ble_gatts_char_handles_t	imu_char_handles;
+	uint8_t						uuid_type;
+} ble_smss_t;
+
+
+//typedef void (*ble_smss_led_write_handler_t) (ble_smss_t * p_smss, uint8_t new_state);
+//typedef void (*ble_smss_button_write_handler_t) (ble_smss_t * p_smss, uint8_t button_state);
+//typedef void (*ble_smss_press_write_handler_t) (ble_smss_t * p_smss, uint8_t press_value);
+//typedef void (*ble_smss_imu_write_handler_t) (ble_smss_t * p_smss, uint8_t imu_value);
+							  
+
+/**@brief Function for initializing the LED Button Service.
+ *
+ * @param[out] p_lbs      LED Button Service structure. This structure must be supplied by
+ *                        the application. It is initialized by this function and will later
+ *                        be used to identify this particular service instance.
+ *
+ * @retval NRF_SUCCESS If the service was initialized successfully. Otherwise, an error code is returned.
+ */
+void ble_smss_init(ble_smss_t * p_smss);
+
+/**@brief Function for handling the application's BLE stack events.
+ *
+ * @details This function handles all events from the BLE stack that are of interest to the LED Button Service.
+ *
+ * @param[in] p_lbs      LED Button Service structure.
+ * @param[in] p_ble_evt  Event received from the BLE stack.
+ */
+void ble_smss_on_ble_evt(ble_smss_t * p_smss, ble_evt_t * p_ble_evt);
+
+/**@brief Function for sending a button state notification.
+ *
+ * @param[in] p_lbs      LED Button Service structure.
+ * @param[in] button_state  New button state.
+ *
+ * @retval NRF_SUCCESS If the notification was sent successfully. Otherwise, an error code is returned.
+ */
+uint32_t ble_smss_on_button_change(ble_smss_t * p_smss, uint8_t button_state);
+
+uint32_t ble_smss_on_imu_value(ble_smss_t * p_smss, uint8_t imu_value);
+
+uint32_t ble_smss_on_press_value(ble_smss_t * p_smss, uint8_t press_value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BLE_SMSS_H__
+
+/** @} */
